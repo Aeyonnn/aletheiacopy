6309321b563dc4f049a4f2ae39b8fdec0a828103
Elnathan John L. Salavarria <ejsalavarria84@gmail.com>
2021-11-18 21:05:07 +0800

    Included main.py for extra functions of the webscraper

--- scraper.py
+++ scraper.py
@@ -5,50 +5,40 @@ from selenium.webdriver.common.keys import Keys
 # op = webdriver.ChromeOptions()
 # op.add_argument('headless')
 # driver = webdriver.Remote(options=op)
-driver = webdriver.Chrome()
-driver.maximize_window()
-driver.get("https://empirenews.net/")
 
-sleep(3)
-result = driver.find_elements_by_xpath('//*[@class="suppaMenu"]')
-
-for item in result:
-    try:
-        # Entertainment Section
+def entertainmentSection():
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    driver.get("https://empirenews.net/")
+    sleep(3)
+    result = driver.find_elements_by_xpath('//*[@class="suppaMenu"]')
+    for item in result:
         try:
-            link = item.find_element_by_link_text('Entertainment').click()
-            sleep(5)
-            scan = driver.find_element_by_partial_link_text(' Older posts')
-            sleep(5)
-            print('scan')
-            while scan:
-                try:
+            # Entertainment Section
+            try:
+                link = item.find_element_by_link_text('Entertainment').click()
+                sleep(5)
+                scan = driver.find_element_by_partial_link_text(' Older posts')
+                sleep(5)
+                print('scan')
+                while scan:
                     content = driver.find_elements_by_xpath('//*[@class="entry-header"]')
                     for data in content:
                         headline = data.find_element_by_class_name('entry-title').get_attribute('innerText')
                         print(headline)
-
                     content = driver.find_elements_by_xpath('//*[@class="entry-content"]')
                     for data in content:
                         paragraph = data.find_elements_by_tag_name('p')
                         for body in paragraph[2:]:
                             print(body.text)
                         print('\n')
-                    scan = driver.find_element_by_partial_link_text(' Older posts').click()
                     sleep(5)
-
-                except:
-                    print('error1')
-
+                    driver.find_element_by_partial_link_text(' Older posts').click()
+                    sleep(5)
+            except:
+                print('error1')
 
         except:
-            print('error1')
+            print(driver.error_handler)
 
-        # Sports Section
-        try:
-            link = item.find_element_by_link_text('Sports').click()
-        except:
-            print('error2')
 
-    except:
-        print('error')
