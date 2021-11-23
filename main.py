d05a2f08659613c992be7f86308a727f9307c804
Elnathan John Salavarria <80999448+elnathan84@users.noreply.github.com>
2021-11-23 19:40:13 +0800

    Delete main.py

--- main.py
+++
@@ -1,4 +0,0 @@
-# Empire News Webscraper -- WARNING this is a fake news website every data contained and gather by this webscraper is only for data algorithm usage.\
-from scraper import entertainmentSection
-
-entertainmentSection()
