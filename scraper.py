934f8cc35f87e7856f9317008b08ae1e2bb6814b
Elnathan John L. Salavarria <ejsalavarria84@gmail.com>
2021-11-23 19:25:18 +0800

    Included a 'Next Page' checker until data is exhausted

--- scraper.py
+++ scraper.py
@@ -6,7 +6,7 @@ from selenium.webdriver.firefox.options import Options
 # op = webdriver.ChromeOptions()
 # op.add_argument('headless')
 # driver = webdriver.Remote(options=op)
-link = []
+
 def politicsSection():
     # options = Options()
     # options.headless = True
@@ -25,12 +25,13 @@ def politicsSection():
             sleep(1)
             driver.find_element_by_partial_link_text('News In Brief').click()
             sleep(1)
-            list = driver.find_elements_by_xpath('//*[@class="cw4lnv-0 iTueKC js_post_item"]')
-            print(len(list))
-            root = driver.current_url
-            link.append(root)
             next = driver.find_element_by_xpath('.//a[contains(@class, "peggds-2 hwNppf next-button")]')
             while next:
+                link = []
+                list = driver.find_elements_by_xpath('//*[@class="cw4lnv-0 iTueKC js_post_item"]')
+                print(len(list))
+                root = driver.current_url
+                link.append(root)
                 for extract in list:
                     link.append(extract.find_element_by_xpath('.//a[contains(@class, "sc-1out364-0 hMndXN js_link")]').get_attribute('href'))
                     print(extract.find_element_by_xpath('.//a[contains(@class, "sc-1out364-0 hMndXN js_link")]').get_attribute('href'))
@@ -39,7 +40,6 @@ def politicsSection():
                     sleep(2)
                     title = driver.find_element_by_xpath('//*[@class="sc-1efpnfq-0 bBLibw"]').get_attribute('textContent')
                     print(title)
-                    print('\n')
                     # Extract news and clean data
                     paragraph = driver.find_element_by_xpath('//*[@class="sc-77igqf-0 bOfvBY"]')
                     nstring = paragraph.text
@@ -47,6 +47,7 @@ def politicsSection():
                     loc+=1
                     nstring[loc:]
                     print(nstring[loc:])
+                    print('\n')
                     sleep(2)
                 driver.get(link[0])
                 sleep(2)
