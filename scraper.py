bb164b0e91bb7295b68ef71c87ca6e577c1ba43c
Elnathan John Salavarria <80999448+elnathan84@users.noreply.github.com>
2022-03-09 14:06:58 +0800

    Extracted individual news links

--- scraper.py
+++ scraper.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 from loading_status import loading
 limit = 100
 column_class = 'ms7aY blog-post-description-font blog-card-background-color blog-card-border-color blog-text-color undefined blog-post-category-border-color blog-post-category-post-container blog-post-category-background-color'
-
+title_class = 'zn0o0 VwmRm blog-post-category-title-font blog-post-category-title-color blog-post-title-color post-title blog-hover-container-element-color Zk5w2 blog-post-category-title-color blog-post-category-title-font'
 def columnSection():
     count = 0
     count1 = 0
@@ -38,23 +38,15 @@ def columnSection():
             try:
                 paragraph = data.find_element_by_xpath('.//*[contains(@class, "_1hN1O NwZmu _3EPBy")]')
                 nstring = paragraph.text
-                # loc = nstring.find('—')
-                # loc+=2
-                # print(nstring[loc:])
                 print(nstring)
             except:
                 print('No News Data Detected')
 
-            # # Get news paragraph
-            # try:
-            #     paragraph = driver.find_element_by_xpath('//*[@class="article__writeup"]')
-            #     nstring = paragraph.text
-            #     loc = nstring.find('—')
-            #     loc+=2
-            #     print(nstring[loc:])
-            # except:
-            #     print('No News Data Detected')
-            # print('\n')
-
+            # Get individual url links
+            try:
+                link = data.find_element_by_xpath(f'.//h2[contains(@class, "{title_class}")]//a').get_attribute('href')
+                print(link)
+            except:
+                print(data.error_handler)
     except:
         print(driver.error_handler)
