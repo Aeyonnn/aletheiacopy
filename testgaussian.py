21b80a02c02947a9daf3f8b2aa211f93af5618cf
Vince <vinceldelapena@gmail.com>
2022-03-21 21:30:30 +0800

    commit w/links

--- testgaussian.py
+++ testgaussian.py
@@ -3,3 +3,4 @@ from Algo.gaussian_p import gaussianProcess
 gaussianProcess()
 
 #https://stackabuse.com/scikit-learn-save-and-restore-models/
+#https://towardsdatascience.com/predictive-analytics-time-series-forecasting-with-gru-and-bilstm-in-tensorflow-87588c852915
