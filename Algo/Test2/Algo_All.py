d3fad508cf3ce0679df2991e710c54ab26b582b7
Vince <vinceldelapena@gmail.com>
2022-04-04 16:30:25 +0800

    w/ passive agressive

--- Algo/Test2/Algo_All.py
+++ Algo/Test2/Algo_All.py
@@ -13,6 +13,7 @@ from sklearn.naive_bayes import BernoulliNB
 from sklearn.neural_network import MLPClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import LinearSVC
+from sklearn.linear_model import PassiveAggressiveClassifier
 import joblib
 
 data = pd.read_csv('data/Dataset1(OS)/news.csv')
@@ -165,4 +166,21 @@ def supportVector():
     print("Accuracy of SVM Classifier: {}%".format(round(accuracy_score(y_test, svc_pred)*100,2)))
 
     joblib.dump(model_svc, f'model/{target}/{test}/aletheia-supportvector.pkl')
+
+def passiveAgressiveClassifier():
+    #Train-test split
+    x_train,x_test,y_train,y_test,z_train,z_test = train_test_split(data['title'], data['label'], data['title'], test_size=0.2, random_state=1)
+
+    #Support Vector classification
+    pipe2 = Pipeline([('vect', CountVectorizer()), ('tfidf', TfidfTransformer()), ('model', PassiveAggressiveClassifier())])
+
+    model_pac = pipe2.fit(x_train, y_train)
+    pac_pred = model_pac.predict(x_test)
+
+    print("\nConfusion Matrix of Passive Agressive Classifier:\n")
+    print(confusion_matrix(y_test, pac_pred))
+    print("\nClassification Report of Passive Agressive Classifier:\n")
+    print(classification_report(y_test, pac_pred))
+    print("Accuracy of Passive Agressive Classifier: {}%".format(round(accuracy_score(y_test, pac_pred)*100,2)))
     
+    joblib.dump(model_pac, f'model/{target}/{test}/aletheia-passiveagressive.pkl')
