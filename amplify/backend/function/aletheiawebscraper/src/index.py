946a16b0a437f0116226d78634b78c06fd296591
Elnathan John Salavarria <80999448+elnathan84@users.noreply.github.com>
2022-05-17 09:51:52 +0800

    updated changes in the webscraper Lambda

--- amplify/backend/function/aletheiawebscraper/src/index.py
+++ amplify/backend/function/aletheiawebscraper/src/index.py
@@ -1,19 +1,22 @@
 import json
+from urllib import response
 from universal_scraper.scraper import get_page_text
 import boto3 
 def handler(event, context):
-  news_scrape = event['news']
-  print('received event:')
-  print(news_scrape)
-
-  print(get_page_text(news_scrape))
-  
-  return {
-      'statusCode': 200,
-      'headers': {
-          'Access-Control-Allow-Headers': '*',
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
-      },
-      'body': json.dumps('Hello from your new Amplify Python lambda!')
+  news_scrape = event['queryStringParameters']['newslink']
+  body = {
+    "news": news_scrape,
+    "newsart": get_page_text(news_scrape)
   }
+
+  response = {
+    'statusCode': 200,
+    'body': json.dumps(body),
+    'headers': {
+        'Access-Control-Allow-Headers': '*',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+      }
+  }
+  
+  return response
