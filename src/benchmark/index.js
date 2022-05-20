9f56a6b99d8abab7b595603bdeead7b1e48846d7
IKYNCHI <kynchsilao@gmail.com>
2022-05-20 20:51:35 +0800

    adjusted indention

--- src/benchmark/index.js
+++ src/benchmark/index.js
@@ -4,13 +4,11 @@ import './table.css'
 import { Formik, Field, Form } from 'formik';
 import CircularProgress from '@mui/material/CircularProgress';
 import Table from 'react-bootstrap/Table'
-//KYNCH wag galaw
 import { API } from 'aws-amplify';
 import Amplify, { Auth } from 'aws-amplify';
 import awsconfig from '../aws-exports';
 Amplify.configure(awsconfig);
 Auth.configure(awsconfig);
-//Up to here
 
 function Progress() {
   // Variables to extract algorithm model predictions
@@ -20,6 +18,7 @@ function Progress() {
   const [randomf, getRandomf] = useState(null)
   const [news_art, getNewsArt] = useState(null)
   const [prediction, setprediction] = useState(null)
+  //Loading Spinner
   const [loading, setLoading] = useState(false)
   const handleClick = () => {
     setLoading(true)
@@ -28,45 +27,40 @@ function Progress() {
       setLoading(false)
     }, 5000)
   }
+  //Prediction Function
   const getPredict = {
     queryStringParameters: {
       news: ""
     }
   };
-
   const getNews = {
     queryStringParameters: {
       newslink: ""
     }
   };
+  //Calling API to get results
   async function fetchNewsAlgo(article){
-    //all functions answer are either true or fake, manually input the names of the model.
-    //show news use news_art variable
-    //combination,decision,neural,randomf
     getPredict.queryStringParameters.news = article
     const apiData = await API.get('algoapi', '/pythonapi', getPredict)
     getCombination(apiData.combination)
     getDecision(apiData.decision)
     getNeural(apiData.neural)
     getRandomf(apiData.randomf)
-
     setprediction(apiData)
   }
-
   async function fetchNewsArt(link){
     getNews.queryStringParameters.newslink = link
     const apiData = await API.get('algoapi', '/aletheiawebscraper-dev', getNews)
     getNewsArt(apiData.newsart)
     await fetchNewsAlgo(apiData.newsart)
   }
-
   window.onload = setprediction;
-
   useEffect(() => {
     // fetchNewsArt()
     // fetchNewsAlgo()
     setprediction()
   }, [prediction])
+
   return (
     <>
     <Container>
@@ -77,67 +71,63 @@ function Progress() {
                {/* Div Input */}
                <SearchInputs>
                <Formik
-      initialValues={{
-        newsSubmit: '',
-      }}
-      onSubmit=
-      {async (values, actions) => {
-        await new Promise((r) => setTimeout(r, 1000));
-        getNews.queryStringParameters.newslink = values.newsSubmit;
+                initialValues={{
+                newsSubmit: '',
+                }}
+                onSubmit=
+                {async (values, actions) => {
+                await new Promise((r) => setTimeout(r, 1000));
+                getNews.queryStringParameters.newslink = values.newsSubmit;
 
-        fetchNewsArt(values.newsSubmit);
-        // getPredict.queryStringParameters.news = news_art;
-        // console.log(getPredict.queryStringParameters.news);
-        // fetchNewsAlgo();
-
-        // console.log(prediction);
-        // alert(JSON.stringify(news_art+neural, null, 2));
-        actions.setSubmitting(false);
-      }}
-   >  
-     {({isSubmitting}) => (
-      <Form>
-        <label htmlFor="newsSubmit"></label>
-        <Field id="newsSubmit" name="newsSubmit" placeholder="Enter News Here" />
-        <button id="submit" type="submit" disabled={isSubmitting} onClick={handleClick}> 
-          Submit</button>
-      </Form>
-     )}
-    </Formik>
-               </SearchInputs>
-             </Search>
-            </FormContent>
-            <FormLoader>
-        {loading ? (<CircularProgress/>) : ("") }
-        </FormLoader>
-            <FormContentTable>
-            {combination ? (
-            <Table striped bordered hover size="sm">
-                  <thead>
-                    <tr>
-                      <th>Model</th>
-                      <th>Result</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <tr>
-                      <td>Combination</td>
-                      <td>{combination}</td>
-                    </tr>
-                    <tr>
-                    <td>Decision Tree</td>
-                    <td>{decision}</td>
-                    </tr>
-                    <tr>
-                    <td>Neural Network</td>
-                    <td>{neural}</td>
-                    </tr>
-                    <tr>
-                    <td>Random Forest</td>
-                    <td>{randomf}</td>
-                    </tr>
-                    </tbody>
-                    </Table>) : 
+                fetchNewsArt(values.newsSubmit);
+                actions.setSubmitting(false);
+                }}>  
+                {({isSubmitting}) => (
+                <Form>
+                <label htmlFor="newsSubmit"></label>
+                  <Field id="newsSubmit" name="newsSubmit" placeholder="Enter News Here" />
+                  <button id="submit" type="submit" disabled={isSubmitting} onClick={handleClick}> 
+                  Submit
+                  </button>
+                </Form>
+                )}
+              </Formik>
+            </SearchInputs>
+          </Search>
+        </FormContent>
+      <FormLoader>
+        {//shows loading screen
+        loading ? (<CircularProgress/>) : ("") }
+      </FormLoader>
+      <FormContentTable>
+        {//shows table
+        combination ? (
+          <Table striped bordered hover size="sm">
+            <thead>
+                <tr>
+                  <th>Model</th>
+                  <th>Result</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                  <td>Combination</td>
+                  <td>{combination}</td>
+                </tr>
+                <tr>
+                  <td>Decision Tree</td>
+                  <td>{decision}</td>
+                </tr>
+                <tr>
+                  <td>Neural Network</td>
+                  <td>{neural}</td>
+                </tr>
+                <tr>
+                  <td>Random Forest</td>
+                  <td>{randomf}</td>
+                </tr>
+            </tbody>
+          </Table>) : 
                     ("")}
         </FormContentTable>
         </FormWrap>
