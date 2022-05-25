ba210824e04fb86d84b021016bf2557ca4241b77
IKYNCHI <kynchsilao@gmail.com>
2022-05-25 21:11:39 +0800

    with button

--- src/benchmark/index.js
+++ src/benchmark/index.js
@@ -208,10 +208,10 @@ function Progress({ signOut, user }) {
                   </table>
                   <div>
                     <p>Is it true?</p>
-                    <button id="submit" type="submit" disabled={isSubmitting} onClick={handleClick}> 
+                    <button id="submit" type="submit" onClick={handleClick}> 
                           Submit
                           </button>
-                          <button id="submit" type="submit" disabled={isSubmitting} onClick={handleClick}> 
+                          <button id="submit" type="submit"  onClick={handleClick}> 
                           Submit
                           </button>
                   </div>
