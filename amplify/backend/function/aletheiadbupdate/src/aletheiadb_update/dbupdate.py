ac113042201f80811de0b09669d1420d5a1a853b
Elnathan John Salavarria <80999448+elnathan84@users.noreply.github.com>
2022-05-31 21:52:36 +0800

    Updates in the API

--- amplify/backend/function/aletheiadbupdate/src/aletheiadb_update/dbupdate.py
+++ amplify/backend/function/aletheiadbupdate/src/aletheiadb_update/dbupdate.py
@@ -1,8 +1,6 @@
 import mysql.connector
 import json
 
-# user = 'vinceldelapena@gmail.com'
-
 def dbUpdate(check, news_id):
   mydb = mysql.connector.connect(
   host="database-1.cyqaefb6grs6.ap-southeast-1.rds.amazonaws.com",
@@ -22,11 +20,3 @@ def dbUpdate(check, news_id):
     return 'Updated Successfully'
   else:
     return 'Update Failed'
-  # return{
-  #   'statusCode': 200,
-  #   'headers': { 'Content-Type': 'application/json' },
-  #   'body': json.dumps({ 'id': '{}'.format(id) })
-  # }
-
-
-# RETURN ID TO REACT NEEDS TO BE CATCH BY A VARIABLE
