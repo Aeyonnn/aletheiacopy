4b0c34448fe6b45dc6f792a4a44b5b8f2744315a
Elnathan John Salavarria <80999448+elnathan84@users.noreply.github.com>
2022-09-24 14:01:36 +0800

    Included imports from adesense

--- src/App.js
+++ src/App.js
@@ -5,7 +5,11 @@ import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import bmpage from './pages/bmpage';
 import aboutpage from './pages/abtpage';
 import privacyact from './pages/privacyact';
-
+import {Adsense} from '@ctrl/react-adsense';
+<Adsense
+  client="ca-pub-7640562161899788"
+  slot="7259870550"
+/>
 function App() {
   return (
     <Router>
